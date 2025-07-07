4b139c68cdee5a11d2acdc1150b05eb576a1fbdc
赵泽浩 <3516422381@qq.com>
2025-07-07 15:52:21 +0800

    进度条

---
+++ main.c
@@ -0,0 +1,12 @@
+#include"processbar.h"
+#include<unistd.h>
+
+int main()
+{
+//	printf("hello world\n");
+//	fflush(stdout);
+//	sleep(2);
+
+	processbar();
+	return 0;
+} 
---
+++ processbar.h
@@ -0,0 +1,10 @@
+#pragma once
+#include<stdio.h>
+#define NUM 102
+#define STYLE '-'
+ 
+extern void processbar();
+
+
+
+
