22de9e6efb91a8f1cba126677894a3bbb728552c
赵泽浩 <3516422381@qq.com>
2025-12-27 11:59:00 +0800

    cmake的基本使用

---
+++ cmakeStudy/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/zzh/linux/cmakeStudy/cmake_tools
+# Build directory: /home/zzh/linux/cmakeStudy/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(testAdd "/home/zzh/linux/cmakeStudy/build/main")
+set_tests_properties(testAdd PROPERTIES  _BACKTRACE_TRIPLES "/home/zzh/linux/cmakeStudy/cmake_tools/CMakeLists.txt;9;add_test;/home/zzh/linux/cmakeStudy/cmake_tools/CMakeLists.txt;0;")
---
+++ cmakeStudy/cmake_tools/Main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<cassert>
+
+int main()
+{
+    assert(1+2 == 3);
+    std::cout<< " test OK " << std::endl;
+    return 0;
+}
